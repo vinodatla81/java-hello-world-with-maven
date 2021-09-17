f56498ff39f08e6c398873e0867be2b35422fb42
root <root@ip-172-31-2-224.ec2.internal>
2021-09-17 02:33:23 +0000

    webhook testing

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
     public String sayHello() {
-        return "BecomeDevops, Github triggered by poll scm on SEP17 2ND";
+        return "BecomeDevops, Github triggered based on the push event";
     }
 }
