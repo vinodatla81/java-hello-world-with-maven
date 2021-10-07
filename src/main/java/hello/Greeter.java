87bac353940ca600669151cb64c252e57ed8c18c
root <you@example.com>
2021-10-07 17:25:49 +0000

    src check

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
     public String sayHello() {
-        return "BecomeDevops, Github webhook";
+        return "BecomeDevops, Github webhook setup"
     }
 }
